15e271f1d277cd2023db2a760bc63fbf13b56801
darnok45 <parragenaro429@gmail.com>
2024-10-15 22:17:16 -0300

    anda la pagina, pero falta arregalr para que le ande boostrap

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './componets/footer/footer/footer.component';
+
+// COMPONENTES LOCALES
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+// COMPONENTES DE MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 
+// ACCEDEMOS A TODAS LAS RUTAS DEL PROYECTO
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [
-    FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule
   ]
 })
 export class SharedModule { }
