bd6fd0476519743e09039e0ce6313b9dfe0604ad
GaugeZich <guilleop.vale@gmail.com>
2024-11-11 12:43:05 -0300

    Diseñe las cards e hice pruebas con datos genericos

--- src/app/modules/producto/components/card/card.component.ts
+++ src/app/modules/producto/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  coleccionProductos: Producto[] = [];
+
+  /* ngOnInit(): void {
+    this.coleccionProductos = [
+      {
+        idProducto: '1',
+        nombre: 'Producto A',
+        precio: 10.99,
+        descripcion: 'Este es el producto A',
+        categoria: 'Categoría 1',
+        imagen: 'https://via.placeholder.com/150',
+        alt: 'Imagen del producto A'
+      },
+      {
+        idProducto: '2',
+        nombre: 'Producto B',
+        precio: 15.99,
+        descripcion: 'Este es el producto B',
+        categoria: 'Categoría 2',
+        imagen: 'https://via.placeholder.com/150',
+        alt: 'Imagen del producto B'
+      },
+      {
+        idProducto: '3',
+        nombre: 'Producto C',
+        precio: 15.99,
+        descripcion: 'Este es el producto B',
+        categoria: 'Categoría 2',
+        imagen: 'https://via.placeholder.com/150',
+        alt: 'Imagen del producto B'
+      },
+      {
+        idProducto: '4',
+        nombre: 'Producto D',
+        precio: 15.99,
+        descripcion: 'Este es el producto B',
+        categoria: 'Categoría 2',
+        imagen: 'https://via.placeholder.com/150',
+        alt: 'Imagen del producto B'
+      }
+    ];
+  } */
 
 }
