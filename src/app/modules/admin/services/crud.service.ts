5d001a994fccdaa59e5834f23411808d239eed06
GaugeZich <guilleop.vale@gmail.com>
2024-11-12 13:02:22 -0300

    Modifique ligeramente tamaño de la barra lateral de administración e hice la tabla para ver los usuarios, editarlos y borrarlos. Falta la funcionalidad de agregar usuarios manualmente

--- src/app/modules/admin/services/crud.service.ts
+++ src/app/modules/admin/services/crud.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Producto } from 'src/app/models/producto';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+import { Usuario } from 'src/app/models/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,11 @@ export class CrudService {
   // Definimos colección para los productos de la web
   private productosCollection: AngularFirestoreCollection<Producto>
 
+  private usuariosCollection: AngularFirestoreCollection<Usuario>
+
   constructor(private database: AngularFirestore) {
     this.productosCollection = database.collection('producto');
+    this.usuariosCollection = database.collection('usuarios')
   }
 
   // CREAR productos
@@ -64,6 +68,64 @@ export class CrudService {
       }
       catch(error){
         reject (error);
+      }
+    })
+  }
+
+
+
+/*
+  Función para crear usuarios desde administración
+*/
+
+
+  crearUsuario(usuario: Usuario){
+    return new Promise(async(res,rej) => {
+      try{
+        // Genera un ID para el usuario
+        const idUsuario = this.database.createId();
+
+        // Lo asigna a UID
+        usuario.uid = idUsuario;
+
+        // Agrega el usuario a firestore
+        const resultado = await this.usuariosCollection.doc(idUsuario).set(usuario)
+
+        res(resultado)
+      }
+      catch(error){
+        rej(error)
+      }
+    })
+  }
+
+  // CRUD -> Función para obtener un usuario
+  /*
+    snapshotChanges -> Toma captura del estado de los datos
+    pipe -> funciona como tuberia, retorna el nuevo arreglo
+    map -> "mapea" o recorre esa nueva información
+    a -> resguarda la nueva información y la envia
+  */
+  obtenerUsuario(){
+    return this.usuariosCollection.snapshotChanges().pipe(map(Action => Action.map(a => a.payload.doc.data())))
+  }
+
+  // CRUD -> Función para editar un usuario
+  modificarUsuario(uid: string, nuevaData: Usuario){
+    // Actualiza los datos del usuario en la colección con la funcion .update
+    return this.database.collection('usuarios').doc(uid).update(nuevaData);
+  }
+  
+  // CRUD -> Función para eliminar un usuario
+  eliminarUsuario(uid: string){
+    return new Promise((resolve, reject) => {
+      try{
+        // Elimina un usuario por el ID utilizando el metodo .delete
+        const resp = this.usuariosCollection.doc(uid).delete()
+        resolve(resp)
+      }
+      catch(error){
+        reject(error)
       }
     })
   }
